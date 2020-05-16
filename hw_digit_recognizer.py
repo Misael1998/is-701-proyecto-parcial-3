63d9f7d41733045a9e38e3ba4fb0a61ff7d3f283
Rony-R <ronyrodriguez2011@hotmail.com>
2020-05-16 11:50:30 -0600

    comezando la vaina

--- hw_digit_recognizer.py
+++ hw_digit_recognizer.py
@@ -1,9 +1,33 @@
 
+import pandas as pd
+import numpy as np
+
 class HWDigitRecognizer:
 
   def __init__(self, train_filename, test_filename):
-    """El método init leerá los datasets con extensión ".csv" cuyas ubicaciones son recibidas mediante los paramentros <train_filename> y <test_filename>. Los usará para crear las matrices de X_train, X_test, Y_train y Y_test con las dimensiones adecuadas y normalización de acuerdo a lo definido en clase para un problema de clasificación multiclase resuelto media una única red neuronal.
+    """El método init leerá los datasets con extensión ".csv" cuyas ubicaciones son recibidas mediante 
+    los paramentros <train_filename> y <test_filename>. Los usará para crear las matrices de X_train, 
+    X_test, Y_train y Y_test con las dimensiones adecuadas y normalización de acuerdo a lo definido en 
+    clase para un problema de clasificación multiclase resuelto media una única red neuronal.
     """
+
+    testData = pd.read_csv(test_filename)
+    trainData = pd.read_csv(train_filename)
+
+    y_train_len = len(trainData.label)
+    y_test_len = len(testData.label)
+
+    y_train = (trainData.label.to_numpy()).reshape(1, y_train_len)
+    y_test = (testData.label.to_numpy()).reshape(1, y_test_len)
+
+    x_train = trainData.drop('label', axis=1).to_numpy().T
+    x_test = testData.drop('label', axis=1).to_numpy().T
+
+    self.X_train = x_train/255
+    self.X_test = x_test/255
+    self.Y_train = y_train
+    self.Y_test = y_test
+
     pass
     
   def train_model(self):
@@ -12,15 +36,26 @@ class HWDigitRecognizer:
     activación RELU en las primeras L-1 capas y la función Softmax en la última capa de tamaño 10
     para realizar clasificación multiclase. 
 
-    Retorna una tupla cuyo primer elemento es un diccionario que contiene los parámetros W y b de todas las capas del modelo con esta estructura:
+    Retorna una tupla cuyo primer elemento es un diccionario que contiene los parámetros W y b de todas 
+    las capas del modelo con esta estructura:
 
     { "W1": ?, "b1": ?, ... , "WL": ?, "bL": ?}
 
-    donde los signos <?> son sustituidos por arreglos de numpy con los valores respectivos. El valor de L será elegido por el estudiante mediante experimentación. El segundo elemento a retornar es una lista con los costos obtenidos durante el entrenamiento cada 100 iteraciones.
+    donde los signos <?> son sustituidos por arreglos de numpy con los valores respectivos. El valor de 
+    L será elegido por el estudiante mediante experimentación. El segundo elemento a retornar es una 
+    lista con los costos obtenidos durante el entrenamiento cada 100 iteraciones.
 
-    Por razones de eficiencia el autograder revisará su programa usando un dataset más pequeño que el que se proporciona (usted puede hacer lo mismo para sus pruebas iniciales). Pero una vez entregado su proyecto se harán pruebas con el dataset completo, por lo que el diccionario que retorna este método con los resultados del entrenamiento con una precisión mayor al 95% en los datos de prueba debe ser entregado junto con este archivo completado.
+    Por razones de eficiencia el autograder revisará su programa usando un dataset más pequeño que el 
+    que se proporciona (usted puede hacer lo mismo para sus pruebas iniciales). Pero una vez entregado 
+    su proyecto se harán pruebas con el dataset completo, por lo que el diccionario que retorna este 
+    método con los resultados del entrenamiento con una precisión mayor al 95% en los datos de prueba 
+    debe ser entregado junto con este archivo completado.
 
-    Para entregar dicho diccionario deberá guardarlo como un archivo usando el módulo "pickle" con el nombre y extensión "all_params.dict", este archivo deberá estar ubicado en el mismo directorio donde se encuentra  el archivo actual: hw_digit_recognizer.py. El autograder validará que este archivo esté presente y tendra las claves correctas, pero la revisión de la precisión se hará por el docente después de la entrega. La estructura del archivo será la siguiente:
+    Para entregar dicho diccionario deberá guardarlo como un archivo usando el módulo "pickle" con el 
+    nombre y extensión "all_params.dict", este archivo deberá estar ubicado en el mismo directorio 
+    donde se encuentra  el archivo actual: hw_digit_recognizer.py. El autograder validará que este 
+    archivo esté presente y tendra las claves correctas, pero la revisión de la precisión se hará por 
+    el docente después de la entrega. La estructura del archivo será la siguiente:
 
     {
       "model_params": { "W1": ?, "b1": ?, ... , "WL": ?, "bL": ?},
@@ -30,8 +65,10 @@ class HWDigitRecognizer:
       "costs": [0.2356, 0.1945, ... 0.00345]
     }
 
-    <model_params>, es un diccionario que contiene los valores de las L * 2  matrices de parámetros del modelo (numpy array) desde W1 y b1, hasta WL y bL, el valor de L será elegido por el estudiante.
-    <layer_dims>, es una lista con las dimensiones de las L+1 capas del modelo, incluyendo la capa de entrada.
+    <model_params>, es un diccionario que contiene los valores de las L * 2  matrices de parámetros del 
+    modelo (numpy array) desde W1 y b1, hasta WL y bL, el valor de L será elegido por el estudiante.
+    <layer_dims>, es una lista con las dimensiones de las L+1 capas del modelo, incluyendo la capa de 
+    entrada.
     <learning_rate>, el valor del ritmo de entrenamiento.
     <num_iterations>, el número de iteraciones que se usó.
     <costs>, una lista con los costos obtenidos cada 100 iteraciones.
@@ -40,9 +77,11 @@ class HWDigitRecognizer:
 
   def predict(self, X, model_params):
     """
-    Retorna una matriz de predicciones de <(1,m)> con valores entre 0 y 9 que representan las etiquetas para el dataset X de tamaño <(n,m)>.
+    Retorna una matriz de predicciones de <(1,m)> con valores entre 0 y 9 que representan las etiquetas 
+    para el dataset X de tamaño <(n,m)>.
 
-    <model_params> contiene un diccionario con los parámetros <w> y <b> de cada uno de los clasificadores tal como se explica en la documentación del método <train_model>.
+    <model_params> contiene un diccionario con los parámetros <w> y <b> de cada uno de los 
+    clasificadores tal como se explica en la documentación del método <train_model>.
     """
     pass
 
@@ -56,4 +95,13 @@ class HWDigitRecognizer:
     "Y_test": Y_test
     }
     """
+
+    d = {
+      "X_train": self.X_train,
+      "X_test": self.X_test,
+      "Y_train": self.Y_train,
+      "Y_test": self.Y_test
+    }
+    return d
+    
     pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from hw_digit_recognizer import HWDigitRecognizer as HWD
+
+
+dgtr = HWD("datasets/mnist_test.csv", "datasets/mnist_train.csv")
